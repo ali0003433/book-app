cf658f1b36d93135e1e3fee7e27389ab275c2718
Alyssa Liguori <alyssa.liguori@gmail.com>
2018-12-01 14:28:56 -0500

    fixed issue w/ constructor func

--- server.js
+++ server.js
@@ -7,10 +7,6 @@ const app = express();
 require('dotenv').config();
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
-    console.log('The server is now running on port' + PORT);
-});
-
 app.use(express.urlencoded({extended:true}));
 app.use('/public', express.static('public'));
 
@@ -22,17 +18,21 @@ app.get('/', (req,res) => {
 
 app.post('/search', (req, res) => {
     const data = req.body;
+    console.log(req.body);
     let url = 'https://www.googleapis.com/books/v1/volumes?q=+';
-    req.body.param === 'title' ? url +=`intitle:${data.searchText}` : url += `inauthor:${data.searchText}`+data.searchText;
+    req.body.param === 'title' ? url +=`intitle:${data.searchText}` : url += `inauthor:${data.searchText}`;
+    console.log(url)
     superagent.get(url)
         .then(book => {
             let booksArr = book.body.items.map(val => {
                 return new Book(val);
             });
+            console.log(booksArr)
             res.render('pages/results', { books: booksArr});
+            
         })
         .catch((error => {
-            res.status(400).send(error);
+            res.status(400).send('hello error');
         }));
 });
 
@@ -40,7 +40,11 @@ app.get('*', (req,res) => {
     res.status(404).send({status: res.status, message: 'What happened?'});
 });
 
-const Book = book => {
+app.listen(PORT, () => {
+    console.log('The server is now running on port' + PORT);
+});
+
+const Book = function(book){
     this.title = book.volumeInfo.title;
     this.author = book.volumeInfo.authors ? book.volumeInfo.authors[0] : 'none';
 };
